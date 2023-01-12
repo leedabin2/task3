1808af38f5299dd04b7f4f44b803bf083e6b2a68
leedabin2 <12193152@inha.edu>
2023-01-12 18:40:15 +0900

    d commit

--- day1.py
+++ day1.py
@@ -18,3 +18,10 @@ subjects = {"의영": "A+",
 student = input("이름 : ")
 subject = "유체역학"
 print(student,"학생의 ",subject,"성적은",subjects["유체역학"],"입니다")
+
+#old style
+print('%s학생의 %s 과목 성적은 %s입니다' % (student, subject, subjects[subject]))
+#modern style
+print("{0}학생의 {1} 과목 성적은 {2}입니다".format(student, subject, subjects[subject]))
+#f스트링
+print(f'{student}학생의 {subject} 과목 성적은 {subjects[subject]}입니다')
